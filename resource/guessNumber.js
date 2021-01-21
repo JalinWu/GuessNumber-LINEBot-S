4faa029443ca53b6c6c85bdaeae83e6625ea5f6f
JalinWu <lily.wu035@gmail.com>
2021-01-21 19:11:17 +0800

    dev

--- resource/guessNumber.js
+++ resource/guessNumber.js
@@ -67,6 +67,37 @@ var guessNumber = function (bot) {
         // Step.5 根據判斷(大於、小於)設定highNum, lowNum，最後回覆使用者"lowNum~hignNum"
         // Step.6 加入計算次數，根據判斷(等於)，回覆使用者"恭喜答對！一共猜了count次"
 
+        // if (userMsg == 'start' || userMsg == 'restart') {
+        //     answer = Math.floor(Math.random() * 100) + 1; // 1~100
+        //     count = 0;
+        //     console.log(answer);
+
+        //     event.reply('開始吧！');
+        // } else {
+        //     count++;
+        //     if (userMsg > answer) {
+        //         highNum = userMsg;
+        //         event.reply(lowNum + ' ~ ' + highNum);
+
+        //     } else if (userMsg < answer) {
+        //         lowNum = userMsg;
+        //         event.reply(lowNum + ' ~ ' + highNum);
+
+        //     } else if (userMsg == answer) {
+        //         event.reply([
+        //             { type: 'text', text: '恭喜答對！一共猜了' + count + '次' },
+        //             {
+        //                 type: 'sticker',
+        //                 packageId: '11537',
+        //                 stickerId: '52002735'
+        //             }
+        //         ]);
+
+        //     } else {
+        //         event.reply('Oops, 我聽不懂你在說什麼...')
+        //     }
+        // }
+
     });
 }
 
